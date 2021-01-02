de7fb300bd12da91c58c7ccb19af1a25ad2bae76
root <root@localhost.localdomain>
2021-01-02 16:36:52 +0200

    reusing the backdrop to handle sideDrawer show

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import classes from './Layout.module.css'; 
 import Aux from '../../hoc/Auxiliary/Aux';
@@ -7,15 +7,25 @@ import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const Layout = (props) => (
-    <Aux>
-        <Toolbar />
-        <SideDrawer />
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-)
+
+class Layout extends Component {
+    state = {showSideDrawer:true}
+    
+    sideDrwerShowHandler = () => {    
+        this.setState({showSideDrawer:false})
+    }
+    
+    render (){
+        return <Aux>
+            <Toolbar />
+            <SideDrawer show={this.state.showSideDrawer} 
+                        sideDrwerhandler = {this.sideDrwerShowHandler} />
+            <main className={classes.Content}>
+                {this.props.children}
+            </main>
+        </Aux>
+    }
+} 
 
 
 
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,19 +1,31 @@
 import React from 'react';
 
+import Aux from '../../../hoc/Auxiliary/Aux';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import Backdrop from '../../UI/Backdrop/Backdrop';
 
 import classes from './SideDrawer.module.css';
 
-const sideDrawer = (props) => (
-    <div className={classes.SideDrawer}>
-        <div className={classes.Logo}>
-            <Logo />
-        </div>
-        <nav>
-            <NavigationItems />
-        </nav>
-    </div>
+const sideDrawer = (props) => {
+    let attechmentClasses = [classes.SideDrawer,classes.Close]
+    if (props.show){
+        attechmentClasses = [classes.SideDrawer,classes.Open]
+    }
+    return (
+        <Aux>
+            <Backdrop show={props.show} 
+                      puchaseCancel={props.sideDrwerhandler} />
+            <div className={attechmentClasses.join(' ')}>
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
+            </div>
+        </Aux>
 );
+    }
 
 export default sideDrawer;
