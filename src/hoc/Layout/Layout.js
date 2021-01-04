a3dad4c806b2f190910faa48d05c86a25f66b6cd
root <root@localhost.localdomain>
2021-01-04 13:34:47 +0200

    updated folder structure

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react';
 
 import classes from './Layout.module.css'; 
-import Aux from '../../hoc/Auxiliary/Aux';
+import Aux from '../Auxiliary/Aux';
 
-import Toolbar from '../Navigation/Toolbar/Toolbar';
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
 
-import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
 
 
 class Layout extends Component {
