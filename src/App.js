50d5beb782f51acf92b8672ca2bc41dfd2b60138
MasterBDX <masterbdxteam@gmail.com>
2021-04-22 06:30:49 +0200

    added login form

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Layout from './hoc/Layout/Layout';
 import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders';
+import SignUp from './components/Auth/SignUp/SignUp';
+import Login from './components/Auth/Login/Login';
 
 function App() {
   return (
     <div>
       <Layout>
         <Switch>
+          <Route path="/signup" component={SignUp} />
+          <Route path="/login" component={Login} />
           <Route path="/checkout" component={Checkout} />
           <Route path="/orders" component={Orders} />
           <Route path="/" component={BurgerBuilder} />
--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -8,6 +8,8 @@ const navigationItems = (props) => (
     <ul className={classes.NavigationItems}>
          <NavItem link="/" exact>Burger</NavItem>
          <NavItem link="/orders">Orders</NavItem>
+         <NavItem link="/signup">SignUp</NavItem>
+         <NavItem link="/login">Login</NavItem>
     </ul>
 );
 
